7936b6498a16c00e1793f9447f25c8363d54b505
Keifer Higham <keiferhigham@Keifers-MacBook-Pro.local>
2024-10-24 17:28:29 -0600

    add google analytics

--- app/root.jsx
+++ app/root.jsx
@@ -20,6 +20,18 @@ export function Layout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
+         {/* Google Analytics Script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WDXBPEBK3G"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WDXBPEBK3G');
+            `,
+          }}
+        />
       </head>
       <body className="bg-gray-800 text-white">
         {children}
